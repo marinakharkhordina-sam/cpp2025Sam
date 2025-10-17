fe9eeab0b318a0a394872aa2298891c06c5bd35f
(сэм) Марина Хархордина <kharkhordinam@gmail.com>
2025-10-18 00:14:15 +0300

    mini hw task 1
    please, don't hit me :<

---
+++ sem1/KharkhordinaMI/homework_1/task_1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+double square(double side)
+{
+	return side * side;
+}
+
+int main()
+{
+	double side;
+	std::cout << "length side: ";
+	std::cin >> side;
+
+	double area = square(side);
+	std::cout << "area = " << area << std::endl;
+	return 0;
+
+}
